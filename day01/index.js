e6000e1aa488e79c24da79c9a9555e4ac2d3eaba
Pär Tjärnberg <par.tjarnberg@cygni.se>
2022-12-01 16:30:29 +0100

    cleanup

--- day01/index.js
+++ day01/index.js
@@ -10,6 +10,5 @@ const getSolutionPart2 = (caloriesPerElf) => { // 206582
 
 let caloriesPerElf = fs.readFileSync("input.txt").toString().trim().split('\n\n').map(elf =>
     elf.split('\n').map(caloriesString => parseInt(caloriesString))
-        .reduce((total, calories) => total + calories, 0)
-);
+        .reduce((total, calories) => total + calories, 0));
 console.log(process.env.part === "part1" ? getSolutionPart1(caloriesPerElf) : getSolutionPart2(caloriesPerElf));

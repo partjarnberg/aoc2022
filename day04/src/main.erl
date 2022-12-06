9d25e71f827d9bad9e5f35a1f1c0964bc9de33aa
Pär Tjärnberg <par.tjarnberg@cygni.se>
2022-12-06 09:36:06 +0100

    Better naming

--- day04/src/main.erl
+++ day04/src/main.erl
@@ -3,7 +3,7 @@
 -export([start/0, solve/0]).
 
 start() ->
-  Input = read_lines_into_list("input.txt"),
+  Input = parse_input("input.txt"),
   Worker = spawn(main, solve,[]),
   case os:getenv("part", "part1") of
     "part1" -> Worker ! {part1, Input};
@@ -12,25 +12,25 @@ start() ->
 
 solve() ->
   receive
-    {part1, Input} -> erlang:display(length(lists:filter(fun ([Head|[Tail]]) -> find_all(Head, Tail) end, Input)));
-    {part2, Input} -> erlang:display(length(lists:filter(fun ([Head|[Tail]]) -> find_any(Head, Tail) end, Input)))
+    {part1, Input} -> erlang:display(length(lists:filter(fun ([First_elf |[Second_elf]]) -> find_all(First_elf, Second_elf) end, Input)));
+    {part2, Input} -> erlang:display(length(lists:filter(fun ([First_elf |[Second_elf]]) -> find_any(First_elf, Second_elf) end, Input)))
   end.
 
-find_all(First, Second) ->
-  case lists:all(fun (Elem) -> lists:member(Elem, Second) end, First) of
+find_all(First_elf_sections, Second_elf_Sections) ->
+  case lists:all(fun (Section) -> lists:member(Section, Second_elf_Sections) end, First_elf_sections) of
     true -> true;
-    false -> lists:all(fun (Elem) -> lists:member(Elem, First) end, Second)
+    false -> lists:all(fun (Elem) -> lists:member(Elem, First_elf_sections) end, Second_elf_Sections)
   end.
 
-find_any(First, Second) ->
-  lists:any(fun (Elem) -> lists:member(Elem, Second) end, First).
+find_any(First_elf_sections, Second_elf_sections) ->
+  lists:any(fun (Elem) -> lists:member(Elem, Second_elf_sections) end, First_elf_sections).
 
-range_in_compartment(Compartment) ->
-  [Head, Tail] = re:split(Compartment, "-"),
-  lists:seq(binary_to_integer(Head), binary_to_integer(Tail)).
+list_of_sections_for_elf(Elf) ->
+  [From, To] = re:split(Elf, "-"),
+  lists:seq(binary_to_integer(From), binary_to_integer(To)).
 
-read_lines_into_list(Filename) ->
+parse_input(Filename) ->
   {ok, Data} = file:read_file(Filename),
-  lists:map(fun(Rucksack) ->
-    lists:map(fun (Compartment) -> range_in_compartment(Compartment) end, Rucksack) end,
+  lists:map(fun(Pair_of_elves) ->
+    lists:map(fun (Elf) -> list_of_sections_for_elf(Elf) end, Pair_of_elves) end,
     lists:map(fun (Row) -> re:split(Row, ",") end, re:split(Data,"\n"))).
